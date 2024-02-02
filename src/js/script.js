3a49641c4e13ea7203d48db84c9bf574c2b34627
Olena Sonhrova <esongrovaa@gmail.com>
2024-02-02 13:57:36 +0200

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,148 @@
+
+const gpickr = new GPickr({
+	el: '.gpickr',
+	stops: [
+		['rgb(255,132,109)', 0],
+		['rgb(255,136,230)', 1]
+	]
+});
+
+
+gpickr.on('change', instance => {
+	const preview = document.querySelector("#blockSize");
+	preview.style.background = instance.getGradient();
+});
+
+
+$('#summernote').summernote({
+	height: 220,
+	focus: true,
+	callbacks: {
+		onChange: function () {
+			var markup = $('.click2edit').summernote('code');
+			$('.resultat').summernote('destroy');
+			document.getElementById('ruseltText').innerHTML = markup;
+		}
+	}
+});
+
+
+function showShopColor() {
+	const shopColorPicker = document.querySelector("shop-color-picker");
+	const displayShopColor = shopColorPicker.style.display;
+	if (displayShopColor == 'block') {
+		shopColorPicker.style.display = 'none';
+	} else {
+		shopColorPicker.style.display = 'block';
+	};
+};
+
+
+function showGradient() {
+	const gradient = document.getElementById('showgradient');
+	const displayGradient = gradient.style.display;
+	if (displayGradient == 'block') {
+		gradient.style.display = 'none';
+	} else {
+		gradient.style.display = 'block';
+	};
+};
+
+
+var inputHeight = document.getElementById('height');
+var inputWidth = document.getElementById('width');
+var blockSize = document.getElementById('blockSize');
+
+
+inputHeight.addEventListener('input', function () {
+	blockSize.style.height = Number(inputHeight.value) + "px";
+});
+
+inputWidth.addEventListener('input', function () {
+	blockSize.style.width = Number(inputWidth.value) + "px";
+});
+
+
+function changeFonColor() {
+	const preview = document.querySelector("#blockSize");
+	preview.style.background = 'transparent';
+
+};
+
+function changeFonOneColor() {
+	const preview = document.querySelector("#blockSize");
+	preview.style.background = '#FFC800';
+};
+
+function changeFonTwoColor() {
+	const preview = document.querySelector("#blockSize");
+	preview.style.background = '#00C8FF';
+};
+
+
+function changeFon() {
+	const preview = document.querySelector("#blockSize");
+	preview.style.background = 'linear-gradient(90deg, rgba(0,18,36,1) 0%, rgba(0,116,255,0.9976365546218487) 100%)';
+};
+
+function changeFonOne() {
+	const preview = document.querySelector("#blockSize");
+	preview.style.background = 'linear-gradient(69deg, rgba(34,193,195,1) 0%, rgba(253,187,45,1) 100%)';
+};
+
+function changeFonTwo() {
+	const preview = document.querySelector("#blockSize");
+	preview.style.background = 'linear-gradient(90deg, rgba(63,94,251,1) 0%, rgba(252,70,107,1) 100%)';
+};
+
+function changeFonThree() {
+	const preview = document.querySelector("#blockSize");
+	preview.style.background = 'linear-gradient(90deg, rgba(131,58,180,1) 0%, rgba(253,29,29,1) 50%, rgba(252,176,69,1) 100%)';
+};
+
+
+
+
+function screenshot() {
+	const captureElement = document.querySelector("#blockSize");
+	html2canvas(captureElement, {
+		allowTaint: true,
+		useCORS: true,
+		backgroundColor: null,
+		scrollY: -window.scrollY
+	}).then((canvas) => {
+		const imageData = canvas.toDataURL("image/png;");
+		const link = document.createElement("a");
+		link.setAttribute("download", "Image.png");
+		link.setAttribute("href", imageData);
+		link.click();
+	});
+};
+
+
+
+
+document.getElementById('myFile').onchange = function () {
+	var url = URL.createObjectURL(this.files[0]);
+	const preview = document.querySelector("#blockSize");
+	preview.style.background = "url(" + url + ")";
+}
+
+function changeFonImage() {
+	const preview = document.querySelector("#blockSize");
+	preview.style.background = "url('/src/image/backgraund1.png')";
+};
+
+function changeFonOneImage() {
+	const preview = document.querySelector("#blockSize");
+	preview.style.background = "url('/src/image/backgraund2.png')";
+};
+
+function changeFonThreeImage() {
+	const preview = document.querySelector("#blockSize");
+	preview.style.background = "url('/src/image/backgraund4.png')";
+};
+
+
+
+
